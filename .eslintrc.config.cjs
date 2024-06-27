4a40fd79ade686bee547c76f3a41e652754b39ff
Yao <35749680+my163cih@users.noreply.github.com>
2024-06-26 23:36:37 -0700

    set up prod image

--- .eslintrc.config.cjs
+++ .eslintrc.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.config.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.config.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
   server: {
     port: 3000,
+    host: true,
   },
 })
